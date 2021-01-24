f5465fd1c43ef95a508ff427ab997f5ef5d7ef2b
vitorjr <vitorufc2013@gmail.com>
2021-01-24 16:35:25 -0300

    Adicionando Infra, Configuration e Repositories

---
+++ ZombieResourcesDDD/Infrastructure/Configuration/ContextBase.cs
@@ -0,0 +1,32 @@
+﻿using Entities.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Configuration
+{
+    public class ContextBase : DbContext
+    {
+        public ContextBase(DbContextOptions<ContextBase> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Resource> Resource { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Se não estiver configurado no projeto IU pega deginição de chame do json configurado
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder.UseSqlServer(GetStringConectionConfig());
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        private string GetStringConectionConfig()
+        {
+            string strCon = "Data Source=DESKTOP-HVNTI80\\DESENVOLVIMENTO;Initial Catalog=DDD_2020_AULA;Integrated Security=False;User ID=sa;Password=1234;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False";
+            return strCon;
+        }
+    }
+}
